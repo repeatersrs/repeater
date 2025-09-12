1efb1572a37347b90ca3e116dfcdac46a6419363
Erik Wessman <erikwessman10@gmail.com>
2025-09-12 17:06:03 +0200

    Fix overdue days text

--- web/src/lib/utils.ts
+++ web/src/lib/utils.ts
@@ -12,3 +12,15 @@ export function formatDateForDisplay(dateString: string): string {
         year: 'numeric',
     });
 }
+
+export function daysSince(date: Date): number {
+    const today = new Date();
+
+    today.setHours(0, 0, 0, 0);
+    date.setHours(0, 0, 0, 0);
+
+    const diffTime = today.getTime() - date.getTime();
+    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+
+    return diffDays;
+}
